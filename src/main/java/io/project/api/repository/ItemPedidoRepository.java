72a5b3800fa5c6ebe9c5a4681a3f101050cb4768
gabriella.x.arruda <axavierga@gmail.com>
2021-08-25 16:30:08 -0300

    Criando repository

---
+++ src/main/java/io/project/api/repository/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package io.project.api.repository;
+
+import io.project.api.model.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
---
+++ src/main/java/io/project/api/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package io.project.api.repository;
+
+import io.project.api.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
