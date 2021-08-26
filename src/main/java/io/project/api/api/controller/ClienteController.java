aa2bc50ae04a9d0cf278db6d23ff7e0edb7b7d7f
gabriella.x.arruda <axavierga@gmail.com>
2021-08-26 18:06:02 -0300

    refactor controller

--- src/main/java/io/project/api/api/controller/ClienteController.java
+++ src/main/java/io/project/api/api/controller/ClienteController.java
@@ -12,15 +12,14 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
     ClienteRepository clienteRepository;
 
-    @GetMapping(value = "/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id) {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getClienteById(@PathVariable Integer id) {
         Optional<Cliente> clienteById = clienteRepository.findById(id);
 
         if (clienteById.isPresent()) {
@@ -29,17 +28,15 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping(value = "/clientes")
-    @ResponseBody
-    public ResponseEntity saveCliente(@RequestBody Cliente cliente) {
+    @PostMapping
+    public ResponseEntity<?> saveCliente(@RequestBody Cliente cliente) {
 
         Cliente clienteById = clienteRepository.save(cliente);
         return ResponseEntity.ok().body(clienteById);
     }
 
-    @DeleteMapping(value = "/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity deleteClienteById(@PathVariable Integer id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteClienteById(@PathVariable Integer id) {
         Optional<Cliente> clienteById = clienteRepository.findById(id);
 
         if (clienteById.isPresent()) {
@@ -49,9 +46,8 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping(value = "/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity updateCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> updateCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
 
         return clienteRepository
                 .findById(id)
@@ -63,8 +59,8 @@ public class ClienteController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/clientes")
-    public ResponseEntity find( Cliente filtro ){
+    @GetMapping
+    public ResponseEntity<?> find( Cliente filtro ){
         ExampleMatcher matcher = ExampleMatcher
                 .matching()
                 .withIgnoreCase()
