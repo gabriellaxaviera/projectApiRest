34bcce4c9c6125210a9323654bef2a070dab509f
gabriella.x.arruda <axavierga@gmail.com>
2021-08-31 11:21:45 -0300

    Lançando excecoes personalizadas

---
+++ src/main/java/io/project/api/exception/ApplicationControllerAdvice.java
@@ -0,0 +1,17 @@
+package io.project.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException regraNegocioException) {
+        String mensagemErro = regraNegocioException.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
