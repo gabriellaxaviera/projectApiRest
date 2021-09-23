275b9ae7a3a0ea0d580cf7f2dbb5e9cdb4f69c39
gabriella.x.arruda <axavierga@gmail.com>
2021-09-23 10:46:54 -0300

    Cria cadastro de novo usuario de autenticacao

--- src/main/java/io/project/api/service/impl/UsuarioServiceImpl.java
+++ src/main/java/io/project/api/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package io.project.api.service.impl;
 
+import io.project.api.domain.model.Usuario;
+import io.project.api.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,23 +9,34 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UsuarioServiceImpl  implements UserDetailsService {
+public class UsuarioServiceImpl implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!username.equals("fulano")){
-            throw new UsernameNotFoundException("Usuário não encontrado na base.");
-        }
+        Usuario usuario = repository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
+
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[]{"USER"};
 
         return User
                 .builder()
-                .username("fulano")
-                .password(encoder.encode("123"))
-                .roles("USER", "ADMIN")
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
                 .build();
     }
 }
