f47f351aa408bd3bcfea8554320b6b0a24c72bed
gabriella.x.arruda <axavierga@gmail.com>
2021-09-24 11:52:07 -0300

    Adiciona swagger ao pom

--- src/main/java/io/project/api/security/JWTAuthFilter.java
+++ src/main/java/io/project/api/security/JWTAuthFilter.java
@@ -1,10 +1,6 @@
 package io.project.api.security;
 
 import io.project.api.service.impl.UsuarioServiceImpl;
-<<<<<<< HEAD
-=======
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> feature/initSecurity
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
